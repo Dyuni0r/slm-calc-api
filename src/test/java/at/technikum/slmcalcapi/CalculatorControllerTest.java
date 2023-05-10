21a4aaf91554c8ca377943ce03d7d8f0dc83313b
Danny do Nascimento <wi22b084@technikum-wien.at>
2023-05-10 15:45:21 +0200

    added unit test for sum feature

--- src/test/java/at/technikum/slmcalcapi/CalculatorControllerTest.java
+++ src/test/java/at/technikum/slmcalcapi/CalculatorControllerTest.java
@@ -1,4 +1,33 @@
 package at.technikum.slmcalcapi;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CalculatorControllerTest {
+    @Test
+    void SumTest1(){
+        CalculatorController calculatorController = new CalculatorController();
+
+        int a = 0, b = 3;
+
+        int res = calculatorController.sum(a, b);
+        assertEquals(3, res);
+    }
+
+    @Test
+    void sumTest2() {
+        CalculatorController calculatorController
+                = new CalculatorController();
+
+        assertEquals(-8, calculatorController.sum(-2, -6));
+    }
+
+    @Test
+    void sumTest3() {
+        CalculatorController calculatorController
+                = new CalculatorController();
+
+        assertEquals(4, calculatorController.sum(-2, 6));
+    }
 }
